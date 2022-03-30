4144ba9db86370366eb6e33b3c5a6dc3cd2466c9
Pedro Luis <pepluisgon01@gmail.com>
2022-03-30 19:52:20 -0300

    31 - Event Basics

--- src/index.js
+++ src/index.js
@@ -79,11 +79,37 @@ function BookList() {
 
 const Book = ({ img, title, author }) => {
   // const { img, title, author } = props;
+
+  // Event - attribute, eventHandler
+  // onClick, onMouseOver
+
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("Hello world");
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Reference example
+      </button>
+      <br />
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example{" "}
+      </button>
       <p>{"Test String"}</p>
     </article>
   );
